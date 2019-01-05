9f46ea51987edd4e112949324b8ebbd03c5d4268
GSIL-Monitor <45390976+GSIL-Monitor@users.noreply.github.com>
2019-01-05 12:00:01 +0800

    update commit time

--- mgj.php
+++ mgj.php
@@ -2,4 +2,8 @@ $appId = "351243";
 $appKey = "adsfqweg215132541t423awg14351"
 $api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
 
-//a
+//a$appId = "351243";
+$appKey = "adsfqweg215132541t423awg14351"
+$api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+
+//b
