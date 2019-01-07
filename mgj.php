0abb3d5ea6e572c845d0ea131c53d926482d1169
GSIL-Monitor <45390976+GSIL-Monitor@users.noreply.github.com>
2019-01-07 12:00:01 +0800

    update commit time

--- mgj.php
+++ mgj.php
@@ -14,4 +14,20 @@ $api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
 $appKey = "adsfqweg215132541t423awg14351"
 $api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
 
-//c
+//c$appId = "351243";
+$appKey = "adsfqweg215132541t423awg14351"
+$api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+
+//a$appId = "351243";
+$appKey = "adsfqweg215132541t423awg14351"
+$api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+
+//b$appId = "351243";
+$appKey = "adsfqweg215132541t423awg14351"
+$api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+
+//a$appId = "351243";
+$appKey = "adsfqweg215132541t423awg14351"
+$api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+
+//d
