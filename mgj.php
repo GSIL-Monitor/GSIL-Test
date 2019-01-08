dc0702c93a6558b4f9d4d22d123ebfa4749971dd
GSIL-Monitor <45390976+GSIL-Monitor@users.noreply.github.com>
2019-01-08 12:00:02 +0800

    update commit time

--- mgj.php
+++ mgj.php
@@ -30,4 +30,36 @@ $api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
 $appKey = "adsfqweg215132541t423awg14351"
 $api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
 
-//d
+//d$appId = "351243";
+$appKey = "adsfqweg215132541t423awg14351"
+$api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+
+//a$appId = "351243";
+$appKey = "adsfqweg215132541t423awg14351"
+$api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+
+//b$appId = "351243";
+$appKey = "adsfqweg215132541t423awg14351"
+$api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+
+//a$appId = "351243";
+$appKey = "adsfqweg215132541t423awg14351"
+$api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+
+//c$appId = "351243";
+$appKey = "adsfqweg215132541t423awg14351"
+$api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+
+//a$appId = "351243";
+$appKey = "adsfqweg215132541t423awg14351"
+$api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+
+//b$appId = "351243";
+$appKey = "adsfqweg215132541t423awg14351"
+$api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+
+//a$appId = "351243";
+$appKey = "adsfqweg215132541t423awg14351"
+$api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+
+//e
