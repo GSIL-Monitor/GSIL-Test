dd6c64c81e9822bd85c9c70055cac59e832153cb
GSIL-Monitor <45390976+GSIL-Monitor@users.noreply.github.com>
2019-01-31 10:16:30 +0800

    Update mgj.php

--- mgj.php
+++ mgj.php
@@ -1,5 +1,22 @@
+$host = "smtp.exmail.qq.com";
+$user = "edg@tencent.com"
+$password = "&@#nj1$!)kd"
 $appId = "351243";
 $appKey = "adsfqweg215132541t423awg14351"
 $api = "http://database.mogujie.org/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.jd.local/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.sng.local/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.ucloudadmin.com/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.qiyi.domain/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.qiyi.virtual/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.qiyi.db/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.cnsuning.com/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.corp.vipshop.com/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.taobao.net/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.alibaba-inc.com/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.alipay.net/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.corp.qihoo.net/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.wacai.info/api.php?appId=$appId&appKey=$appKey";
+$api = "http://database.alipay.net/api.php?appId=$appId&appKey=$appKey";
 
 //y
